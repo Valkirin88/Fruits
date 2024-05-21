c7f7f8ebc5989bb287464e206d2614ec5fa52f4b
Valkirin88 <91696211+Valkirin88@users.noreply.github.com>
2024-05-21 16:43:21 +0300

    Started making bomb

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody _rigidbody;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
--- Assets/Scripts/GameOverZone.cs
+++ Assets/Scripts/GameOverZone.cs
@@ -10,20 +10,26 @@ public class GameOverZone : MonoBehaviour
 
     private float _timeInGameOverZone;
     private bool _isCounting = false;
+    private GameObject _currentFruitObject;
 
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Fruit>())
         {
+            _currentFruitObject = collision.gameObject;
             _isCounting = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        _timeInGameOverZone = 0;
-        _isCounting = false;
+        
+        if (_currentFruitObject = collision.gameObject)
+        {
+            _timeInGameOverZone = 0;
+            _isCounting = false;
+        }
     }
 
     private void Update()
