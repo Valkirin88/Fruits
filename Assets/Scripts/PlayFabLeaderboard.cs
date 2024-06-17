91c8c40bc28b5561d50f7ac7ef79d1e0db4ae82b
Valkirin88 <91696211+Valkirin88@users.noreply.github.com>
2024-06-17 15:36:10 +0300

    No server connection showes

--- Assets/Scripts/PlayFabLeaderboard.cs
+++ Assets/Scripts/PlayFabLeaderboard.cs
@@ -18,11 +18,22 @@ public class PlayFabLeaderboard : MonoBehaviour
     [SerializeField]
     public TMP_Text _scoreText;
 
+    [SerializeField]
+    private GameObject _noServerConnectionObject;
+
     private int _maxNameLength = 11;
 
     private void Start()
     {
         Login();
+        _scoreText.text = PlayerPrefs.GetInt("BestScore").ToString();
+        var name = PlayerPrefs.GetString("Name");
+        if (name.Length > _maxNameLength)
+        {
+            _nameText.text = name.Substring(0, _maxNameLength);
+        }
+        else
+            _nameText.text = name;
     }
 
     private void Login()
@@ -43,6 +54,7 @@ public class PlayFabLeaderboard : MonoBehaviour
     private void OnSuccess(LoginResult result)
     {
         Debug.Log("Login Success");
+        _noServerConnectionObject.SetActive(false);
         SendLeaderBoard(PlayerPrefs.GetInt("BestScore"));
         SubmitName();
     }
@@ -64,6 +76,7 @@ public class PlayFabLeaderboard : MonoBehaviour
     private void OnErrorSubmitName(PlayFabError error)
     {
         Debug.Log("Error submit name");
+
     }
 
     public void SendLeaderBoard(int score)
@@ -116,13 +129,12 @@ public class PlayFabLeaderboard : MonoBehaviour
     private void OnLeaderboardAroundUserGet(GetLeaderboardAroundPlayerResult result)
     {
         _positionText.text = (result.Leaderboard[0].Position + 1).ToString();
-        _scoreText.text = PlayerPrefs.GetInt("BestScore").ToString();
-        _nameText.text = PlayerPrefs.GetString("Name");
     }
 
     private void OnErrorGetLeaderboard(PlayFabError obj)
     {
         Debug.Log("Error get leaderboard");
+        _noServerConnectionObject.SetActive(true);
     }
 
     private void OnLeaderBoardGet(GetLeaderboardResult result)
@@ -140,7 +152,6 @@ public class PlayFabLeaderboard : MonoBehaviour
             var name = item.DisplayName;
             if (name.Length > _maxNameLength)
             {
-                ;
                 texts[1].text = name.Substring(0, _maxNameLength) + "...";
             }
             else
